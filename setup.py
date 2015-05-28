1c8a3c81bdc9cbf421ea9ac611e5cc8950ab8e08
Stephen Stack <Stephen.Stack@digi.com>
2015-05-28 14:17:34 -0500

    dist: very simple setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='wpa_supplicant',
+    version='0.1',
+    description='WPA Supplicant wrapper for Python',
+    author="Stephen Stack",
+    author_email="Stephen.Stack@digi.com",
+    install_requires=open('requirements.txt').read().split(),
+    packages=find_packages(),
+)
