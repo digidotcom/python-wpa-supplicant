291aeada7118251c21d18e7c04d167c5f4909cc5
Stephen Stack <Stephen.Stack@digi.com>
2015-06-02 15:54:27 -0500

    dist: add copyright to all source files

--- wpa_supplicant/test/mocks.py
+++ wpa_supplicant/test/mocks.py
@@ -1,3 +1,9 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Copyright (c) 2015 Digi International Inc. All Rights Reserved.
+
 """This module provides classes for mocking out the txdbus layer at a low level
 
 Mocking out things at this layer means that some of the txdbus code is being tested
