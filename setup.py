9724ba3298887a166e621fbbc81e8b864b4d7b2c
Stephen Stack <Stephen.Stack@digi.com>
2015-06-03 15:54:00 -0500

    dist: fix OS classifier

--- setup.py
+++ setup.py
@@ -62,6 +62,6 @@ setup(
         "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
         "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries",
-        "Operating System :: Linux",
+        "Operating System :: POSIX :: Linux",
     ],
 )
